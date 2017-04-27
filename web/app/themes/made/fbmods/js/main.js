a16cde41d64f5ac6744b6f1ef4e61e6503c0e3b3
Bryant Smith <bryant.robert.smith@gmail.com>
2017-04-27 13:21:23 -0500

    Enable smoothscolling links

--- web/app/themes/made/fbmods/js/main.js
+++ web/app/themes/made/fbmods/js/main.js
@@ -2,12 +2,35 @@
 
 var Main = (function ($) {
   return {
+    smoothScroll: function () {
+
+      // Smoothscroll links
+      $('a.smoothscroll, .smoothscroll a').click(function(e) {
+        e.preventDefault();
+        var href = $(this).attr('href');
+        _scrollBody($(href));
+      });
+
+      function _scrollBody(element, duration, delay) {
+        if ($('#wpadminbar').length) {
+          wpOffset = $('#wpadminbar').height();
+        } else {
+          wpOffset = 0;
+        }
+        console.log('scrolling!');
+        element.velocity("scroll", {
+          duration: duration,
+          delay: delay,
+          offset: -wpOffset
+        }, "easeOutSine");
+      }
+    },
     wireframifyImages: function () {
 
       $('img:not(.no-wireframify)').wrap('<div class="wireframe-image-wrapper"></div>');
       $('.wireframe-image-wrapper').each(function() {
         var $this = $(this);
-        var $image = $this.find('img')
+        var $image = $this.find('img');
         var $wireframeImage = $('<div class="wireframe-image"></div>').appendTo($this);
         $wireframeImage.height($image.height());
 
@@ -22,10 +45,15 @@ var Main = (function ($) {
     initMain: function () {
       $(document).ready(function () {
         Main.wireframifyImages();
-      })
+        Main.smoothScroll();
+      });
     }
   };
 // Pass in jQuery.
 })(jQuery);
 
 Main.initMain();
+
+
+
+
