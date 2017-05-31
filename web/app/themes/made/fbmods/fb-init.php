73decdb90246d813ea729e9f159c81466d48e543
Bryant Smith <bryant.robert.smith@gmail.com>
2017-05-31 17:37:26 -0500

    Whoops forgot to add the php file

---
+++ web/app/themes/made/fbmods/fb-init.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Firebelly\Init;
+
+/**
+ * Don't run wpautop before shortcodes are run! wtf Wordpress. from http://stackoverflow.com/a/14685465/1001675
+ */
+remove_filter('the_content', 'wpautop');
+add_filter('the_content', 'wpautop' , 99);
+add_filter('the_content', 'shortcode_unautop',100);
+
+/**
+ * Various theme defaults
+ */
+function setup() {
+  // Default Image options
+  update_option('image_default_align', 'none');
+  update_option('image_default_link_type', 'none');
+  update_option('image_default_size', 'large');
+}
+add_action('after_setup_theme', __NAMESPACE__ . '\setup');
+
+/*
+ * Tiny MCE options
+ */
+function mce_buttons_2($buttons) {
+  array_unshift($buttons, 'styleselect');
+  return $buttons;
+}
+add_filter('mce_buttons_2', __NAMESPACE__ . '\mce_buttons_2');
+
+function simplify_tinymce($settings) {
+  // What goes into the 'formatselect' list
+  $settings['block_formats'] = 'H2=h2;H3=h3;Paragraph=p';
+
+  $settings['inline_styles'] = 'false';
+  if (!empty($settings['formats']))
+    $settings['formats'] = substr($settings['formats'],0,-1).",underline: { inline: 'u', exact: true} }";
+  else
+    $settings['formats'] = "{ underline: { inline: 'u', exact: true} }";
+  
+  // What goes into the toolbars. Add 'wp_adv' to get the Toolbar toggle button back
+  $settings['toolbar1'] = 'styleselect,bold,italic,underline,strikethrough,formatselect,bullist,numlist,blockquote,link,unlink,hr,wp_more,outdent,indent,AccordionShortcode,AccordionItemShortcode,fullscreen';
+  $settings['toolbar2'] = '';
+  $settings['toolbar3'] = '';
+  $settings['toolbar4'] = '';
+
+  // $settings['autoresize_min_height'] = 250;
+  $settings['autoresize_max_height'] = 1000;
+
+  // Clear most formatting when pasting text directly in the editor
+  $settings['paste_as_text'] = 'true';
+
+  $style_formats = array( 
+    // array( 
+    //   'title' => 'Two Column',
+    //   'block' => 'div',
+    //   'classes' => 'two-column',
+    //   'wrapper' => true,
+    // ),  
+    // array( 
+    //   'title' => 'Three Column',
+    //   'block' => 'div',
+    //   'classes' => 'three-column',
+    //   'wrapper' => true,
+    // ),
+    // array( 
+    //   'title' => 'Button',
+    //   'block' => 'span',
+    //   'classes' => 'button',
+    // ),
+    // array( 
+    //   'title' => '» Arrow Link',
+    //   'block' => 'span',
+    //   'classes' => 'arrow-link',
+    // ),
+ );  
+  $settings['style_formats'] = json_encode($style_formats);
+
+  return $settings;
+}
+add_filter('tiny_mce_before_init', __NAMESPACE__ . '\simplify_tinymce');
