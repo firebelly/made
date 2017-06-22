6a0af413b13aab3f4083cdbd083e0a3720bdf0d5
Bryant Smith <bryant.robert.smith@gmail.com>
2017-06-22 16:02:39 -0500

    Create fallback for 404 page: fallback to home if page-not-found page does not exist

--- web/app/themes/made/404.php
+++ web/app/themes/made/404.php
@@ -1,6 +1,11 @@
 <?php
-// Ran outta time, redirect to home
+// Redirect to page-not-found if it exists, otherwise home
 header("HTTP/1.1 301 Moved Permanently");
-header("Location: ".get_bloginfo('url').'/page-not-found/');
+$page_not_found_page = get_page_by_path('page-not-found',OBJECT);
+if( isset($page_not_found_page) ) {
+  header("Location: ".get_bloginfo('url').'/page-not-found/');
+} else {
+  header("Location: ".get_bloginfo('url'));
+}
 exit();
 ?>
