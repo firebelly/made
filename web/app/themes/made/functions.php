94824ba62e83c980d6e779210e269b0d86a9c799
Nate Beaty <nate@clixel.com>
2019-06-04 15:00:51 -0400

    move main product description below summary on single pages, add woocommerce lightbox support for galleries, redirect to single product if only one is available on an archive page

--- web/app/themes/made/functions.php
+++ web/app/themes/made/functions.php
@@ -37,7 +37,7 @@ include_once( get_stylesheet_directory() . '/lib/woocommerce/woocommerce-notice.
 // Set Localization (do not remove).
 add_action( 'after_setup_theme', 'infinity_localization_setup' );
 function infinity_localization_setup(){
-	load_child_theme_textdomain( 'infinity-pro', get_stylesheet_directory() . '/languages' );
+  load_child_theme_textdomain( 'infinity-pro', get_stylesheet_directory() . '/languages' );
 }
 
 // Child theme (do not remove).
@@ -49,38 +49,38 @@ define( 'CHILD_THEME_VERSION', '1.1.2' );
 add_action( 'wp_enqueue_scripts', 'infinity_enqueue_scripts_styles' );
 function infinity_enqueue_scripts_styles() {
 
-	wp_enqueue_style( 'infinity-fonts', '//fonts.googleapis.com/css?family=Cormorant+Garamond:400,400i,700|Raleway:700', array(), CHILD_THEME_VERSION );
-	wp_enqueue_style( 'infinity-ionicons', '//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css', array(), CHILD_THEME_VERSION );
+  wp_enqueue_style( 'infinity-fonts', '//fonts.googleapis.com/css?family=Cormorant+Garamond:400,400i,700|Raleway:700', array(), CHILD_THEME_VERSION );
+  wp_enqueue_style( 'infinity-ionicons', '//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css', array(), CHILD_THEME_VERSION );
 
-	wp_enqueue_script( 'infinity-match-height', get_stylesheet_directory_uri() . '/js/match-height.js', array( 'jquery' ), '0.5.2', true );
-	wp_enqueue_script( 'infinity-global', get_stylesheet_directory_uri() . '/js/global.js', array( 'jquery', 'infinity-match-height' ), '1.0.0', true );
+  wp_enqueue_script( 'infinity-match-height', get_stylesheet_directory_uri() . '/js/match-height.js', array( 'jquery' ), '0.5.2', true );
+  wp_enqueue_script( 'infinity-global', get_stylesheet_directory_uri() . '/js/global.js', array( 'jquery', 'infinity-match-height' ), '1.0.0', true );
 
-	$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
-	wp_enqueue_script( 'infinity-responsive-menu', get_stylesheet_directory_uri() . '/js/responsive-menus' . $suffix . '.js', array( 'jquery' ), CHILD_THEME_VERSION, true );
-	wp_localize_script(
-		'infinity-responsive-menu',
-		'genesis_responsive_menu',
-		infinity_responsive_menu_settings()
-	);
+  $suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
+  wp_enqueue_script( 'infinity-responsive-menu', get_stylesheet_directory_uri() . '/js/responsive-menus' . $suffix . '.js', array( 'jquery' ), CHILD_THEME_VERSION, true );
+  wp_localize_script(
+    'infinity-responsive-menu',
+    'genesis_responsive_menu',
+    infinity_responsive_menu_settings()
+  );
 
 }
 
 // Define our responsive menu settings.
 function infinity_responsive_menu_settings() {
 
-	$settings = array(
-		'mainMenu'         => __( 'Menu', 'infinity-pro' ),
-		'menuIconClass'    => 'ionicons-before ion-ios-drag',
-		'subMenu'          => __( 'Submenu', 'infinity-pro' ),
-		'subMenuIconClass' => 'ionicons-before ion-chevron-down',
-		'menuClasses'      => array(
-			'others' => array(
-				'.nav-primary',
-			),
-		),
-	);
+  $settings = array(
+    'mainMenu'         => __( 'Menu', 'infinity-pro' ),
+    'menuIconClass'    => 'ionicons-before ion-ios-drag',
+    'subMenu'          => __( 'Submenu', 'infinity-pro' ),
+    'subMenuIconClass' => 'ionicons-before ion-chevron-down',
+    'menuClasses'      => array(
+      'others' => array(
+        '.nav-primary',
+      ),
+    ),
+  );
 
-	return $settings;
+  return $settings;
 
 }
 
@@ -95,11 +95,11 @@ add_theme_support( 'genesis-responsive-viewport' );
 
 // Add support for custom header.
 add_theme_support( 'custom-header', array(
-	'width'           => 400,
-	'height'          => 130,
-	'header-selector' => '.site-title a',
-	'header-text'     => false,
-	'flex-height'     => true,
+  'width'           => 400,
+  'height'          => 130,
+  'header-selector' => '.site-title a',
+  'header-text'     => false,
+  'flex-height'     => true,
 ) );
 
 // Add image sizes.
@@ -127,18 +127,18 @@ remove_filter( 'wp_nav_menu_items', 'genesis_nav_right', 10, 2 );
 // Remove navigation meta box.
 add_action( 'genesis_theme_settings_metaboxes', 'infinity_remove_genesis_metaboxes' );
 function infinity_remove_genesis_metaboxes( $_genesis_theme_settings_pagehook ) {
-	remove_meta_box( 'genesis-theme-settings-nav', $_genesis_theme_settings_pagehook, 'main' );
+  remove_meta_box( 'genesis-theme-settings-nav', $_genesis_theme_settings_pagehook, 'main' );
 }
 
 // Remove skip link for primary navigation.
 add_filter( 'genesis_skip_links_output', 'infinity_skip_links_output' );
 function infinity_skip_links_output( $links ) {
 
-	if ( isset( $links['genesis-nav-primary'] ) ) {
-		unset( $links['genesis-nav-primary'] );
-	}
+  if ( isset( $links['genesis-nav-primary'] ) ) {
+    unset( $links['genesis-nav-primary'] );
+  }
 
-	return $links;
+  return $links;
 
 }
 
@@ -157,18 +157,18 @@ add_action( 'genesis_footer', 'genesis_do_subnav', 5 );
 add_action( 'genesis_before_header', 'infinity_offscreen_content_output' );
 function infinity_offscreen_content_output() {
 
-	$button = '<button class="offscreen-content-toggle"><i class="icon ion-ios-close-empty"></i> <span class="screen-reader-text">' . __( 'Hide Offscreen Content', 'infinity-pro' ) . '</span></button>';
+  $button = '<button class="offscreen-content-toggle"><i class="icon ion-ios-close-empty"></i> <span class="screen-reader-text">' . __( 'Hide Offscreen Content', 'infinity-pro' ) . '</span></button>';
 
-	if ( is_active_sidebar( 'offscreen-content' ) ) {
+  if ( is_active_sidebar( 'offscreen-content' ) ) {
 
-		echo '<div class="offscreen-content-icon"><button class="offscreen-content-toggle"><i class="icon ion-ios-more"></i> <span class="screen-reader-text">' . __( 'Show Offscreen Content', 'infinity-pro' ) . '</span></button></div>';
+    echo '<div class="offscreen-content-icon"><button class="offscreen-content-toggle"><i class="icon ion-ios-more"></i> <span class="screen-reader-text">' . __( 'Show Offscreen Content', 'infinity-pro' ) . '</span></button></div>';
 
-	}
+  }
 
-	genesis_widget_area( 'offscreen-content', array(
-		'before' => '<div class="offscreen-content"><div class="offscreen-container"><div class="widget-area">' . $button . '<div class="wrap">',
-		'after'  => '</div></div></div></div>',
-	));
+  genesis_widget_area( 'offscreen-content', array(
+    'before' => '<div class="offscreen-content"><div class="offscreen-container"><div class="widget-area">' . $button . '<div class="wrap">',
+    'after'  => '</div></div></div></div>',
+  ));
 
 }
 
@@ -176,63 +176,63 @@ function infinity_offscreen_content_output() {
 add_filter( 'wp_nav_menu_args', 'infinity_secondary_menu_args' );
 function infinity_secondary_menu_args( $args ) {
 
-	if ( 'secondary' != $args['theme_location'] ) {
-		return $args;
-	}
+  if ( 'secondary' != $args['theme_location'] ) {
+    return $args;
+  }
 
-	$args['depth'] = 1;
+  $args['depth'] = 1;
 
-	return $args;
+  return $args;
 
 }
 
 // Modify size of the Gravatar in the author box.
 add_filter( 'genesis_author_box_gravatar_size', 'infinity_author_box_gravatar' );
 function infinity_author_box_gravatar( $size ) {
-	return 100;
+  return 100;
 }
 
 // Modify size of the Gravatar in the entry comments.
 add_filter( 'genesis_comment_list_args', 'infinity_comments_gravatar' );
 function infinity_comments_gravatar( $args ) {
 
-	$args['avatar_size'] = 60;
+  $args['avatar_size'] = 60;
 
-	return $args;
+  return $args;
 
 }
 
 // Setup widget counts.
 function infinity_count_widgets( $id ) {
 
-	global $sidebars_widgets;
+  global $sidebars_widgets;
 
-	if ( isset( $sidebars_widgets[ $id ] ) ) {
-		return count( $sidebars_widgets[ $id ] );
-	}
+  if ( isset( $sidebars_widgets[ $id ] ) ) {
+    return count( $sidebars_widgets[ $id ] );
+  }
 
 }
 
 // Determine the widget area class.
 function infinity_widget_area_class( $id ) {
 
-	$count = infinity_count_widgets( $id );
+  $count = infinity_count_widgets( $id );
 
-	$class = '';
+  $class = '';
 
-	if ( $count == 1 ) {
-		$class .= ' widget-full';
-	} elseif ( $count % 3 == 1 ) {
-		$class .= ' widget-thirds';
-	} elseif ( $count % 4 == 1 ) {
-		$class .= ' widget-fourths';
-	} elseif ( $count % 2 == 0 ) {
-		$class .= ' widget-halves uneven';
-	} else {
-		$class .= ' widget-halves';
-	}
+  if ( $count == 1 ) {
+    $class .= ' widget-full';
+  } elseif ( $count % 3 == 1 ) {
+    $class .= ' widget-thirds';
+  } elseif ( $count % 4 == 1 ) {
+    $class .= ' widget-fourths';
+  } elseif ( $count % 2 == 0 ) {
+    $class .= ' widget-halves uneven';
+  } else {
+    $class .= ' widget-halves';
+  }
 
-	return $class;
+  return $class;
 
 }
 
@@ -241,49 +241,49 @@ add_theme_support( 'genesis-footer-widgets', 3 );
 
 // Register widget areas.
 genesis_register_sidebar( array(
-	'id'          => 'front-page-1',
-	'name'        => __( 'Front Page Banner', 'infinity-pro' ),
-	'description' => __( 'This is the front page 1 section.', 'infinity-pro' ),
+  'id'          => 'front-page-1',
+  'name'        => __( 'Front Page Banner', 'infinity-pro' ),
+  'description' => __( 'This is the front page 1 section.', 'infinity-pro' ),
 ) );
 genesis_register_sidebar( array(
-	'id'          => 'front-page-2',
-	'name'        => __( 'Front Page Description', 'infinity-pro' ),
-	'description' => __( 'This is the front page 2 section.', 'infinity-pro' ),
+  'id'          => 'front-page-2',
+  'name'        => __( 'Front Page Description', 'infinity-pro' ),
+  'description' => __( 'This is the front page 2 section.', 'infinity-pro' ),
 ) );
 genesis_register_sidebar( array(
-	'id'          => 'front-page-3',
-	'name'        => __( 'Front Page Project Blog', 'infinity-pro' ),
-	'description' => __( 'This is the front page 3 section.', 'infinity-pro' ),
+  'id'          => 'front-page-3',
+  'name'        => __( 'Front Page Project Blog', 'infinity-pro' ),
+  'description' => __( 'This is the front page 3 section.', 'infinity-pro' ),
 ) );
 genesis_register_sidebar( array(
-	'id'          => 'front-page-4',
-	'name'        => __( 'Front Page Comic Updates', 'infinity-pro' ),
-	'description' => __( 'This is the front page 4 section.', 'infinity-pro' ),
+  'id'          => 'front-page-4',
+  'name'        => __( 'Front Page Comic Updates', 'infinity-pro' ),
+  'description' => __( 'This is the front page 4 section.', 'infinity-pro' ),
 ) );
 // genesis_register_sidebar( array(
-// 	'id'          => 'front-page-5',
-// 	'name'        => __( 'Front Page 5', 'infinity-pro' ),
-// 	'description' => __( 'This is the front page 5 section.', 'infinity-pro' ),
+//  'id'          => 'front-page-5',
+//  'name'        => __( 'Front Page 5', 'infinity-pro' ),
+//  'description' => __( 'This is the front page 5 section.', 'infinity-pro' ),
 // ) );
 // genesis_register_sidebar( array(
-// 	'id'          => 'front-page-6',
-// 	'name'        => __( 'Front Page 6', 'infinity-pro' ),
-// 	'description' => __( 'This is the front page 6 section.', 'infinity-pro' ),
+//  'id'          => 'front-page-6',
+//  'name'        => __( 'Front Page 6', 'infinity-pro' ),
+//  'description' => __( 'This is the front page 6 section.', 'infinity-pro' ),
 // ) );
 // genesis_register_sidebar( array(
-// 	'id'          => 'front-page-7',
-// 	'name'        => __( 'Front Page 7', 'infinity-pro' ),
-// 	'description' => __( 'This is the front page 7 section.', 'infinity-pro' ),
+//  'id'          => 'front-page-7',
+//  'name'        => __( 'Front Page 7', 'infinity-pro' ),
+//  'description' => __( 'This is the front page 7 section.', 'infinity-pro' ),
 // ) );
 // genesis_register_sidebar( array(
-// 	'id'          => 'lead-capture',
-// 	'name'        => __( 'Lead Capture', 'infinity-pro' ),
-// 	'description' => __( 'This is the lead capture section.', 'infinity-pro' ),
+//  'id'          => 'lead-capture',
+//  'name'        => __( 'Lead Capture', 'infinity-pro' ),
+//  'description' => __( 'This is the lead capture section.', 'infinity-pro' ),
 // ) );
 // genesis_register_sidebar( array(
-// 	'id'          => 'offscreen-content',
-// 	'name'        => __( 'Offscreen Content', 'infinity-pro' ),
-// 	'description' => __( 'This is the offscreen content section.', 'infinity-pro' ),
+//  'id'          => 'offscreen-content',
+//  'name'        => __( 'Offscreen Content', 'infinity-pro' ),
+//  'description' => __( 'This is the offscreen content section.', 'infinity-pro' ),
 // ) );
 
 
@@ -299,3 +299,25 @@ include_once( get_stylesheet_directory()  . '/fbmods/fb-metatags.php' );
 
 // Fb init to clean up tinymce
 include_once( get_stylesheet_directory()  . '/fbmods/fb-init.php' );
+
+// Re-enable woocommerce lightbox
+add_action('after_setup_theme', function() {
+  add_theme_support( 'wc-product-gallery-lightbox' );
+  // add_theme_support( 'wc-product-gallery-zoom' );
+  // add_theme_support( 'wc-product-gallery-slider' );
+});
+
+// Move description/info under short description
+remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10);
+add_action('woocommerce_single_product_summary', 'woocommerce_output_product_data_tabs', 60);
+
+// Make
+add_action('template_redirect', function() {
+  global $wp_query;
+  if ((is_shop() || is_product_category() || is_product_tag()) && $wp_query->post_count == 1) {
+    the_post();
+    $post_url = get_permalink();
+    wp_safe_redirect($post_url ,302);
+    exit;
+  }
+});
